cb98c90d922be60ab59befb15c8a86790495b8d5
Melina-mn <142990929+Melina-mn@users.noreply.github.com>
2023-11-12 14:35:50 +0100

    code added in js

--- script.js
+++ script.js
@@ -1,45 +1,3 @@
-
-//TO DO:
-
-//1. the account holders name: should be a string DONE
-const firstName = "Melina";
-console.log(firstName);
-
-
-//2. the object should hold all of our logic DONE
-
-const account = {
-    name: "Melina", 
-}; 
-
-console.log(
-    "Please select from the menu: \n1) Add Income\n2) Add Expences\n3) List All Expences\n4) Get Summery"
-); 
-
-function menu() {
-    const choice = parseFloat(
-        //NOTE TO MYSELF: parseFloat() The parseFloat() 
-        //function parses a string argument and returns a floating point number.
-        prompt(
-            "Please select from the menu: \n1) Add Income\n2) Add Expences\n3) List All Expences\n4) Get Summery"
-        )
-    );
-    console.log("choice: " + choice);
-}
-
-//we shopuld call it out: 
-menu()
-
-
-//3. expences: it can be empty, it should hold all of my expences  
-
-
-
-
-
-
-
-
 // THE ACCOUNT OBJECT SHOULD HAVE THE FOLLWOING PROPERTIES:
 // name: the account holders name, should be a string
 // expenses: an array that should hold all of your expenses, can be empty from start
@@ -59,3 +17,69 @@ menu()
 // depending on what the user selects from the menu you should call on the correct
 // method or poperty from the account object. You can use a switch or if/else statement for your
 // different menu choices. Please motivate you choice in a comment.
+
+
+// TO DO LIST:
+// 1. CREATE AN OBJECT FOR YOUR WEBSITE THAT HOLDS (NAME, EXPENSES, INCOME, ADDEXPENSES,ADDINCOME, LISTALLEXPENSES, AND A SUMMERY)
+const melinaObject = {
+    name: "Melina",
+    expenses: [],
+    income: [],
+}
+console.log(melinaObject);
+
+
+const info = prompt(
+    "Please select from the menu: \n1) Add Income\n2) Add Expences\n3) List All Expences\n4) Get Summery"
+);
+
+
+console.log(info);
+// 2. ADD SWITCH: DONE
+switch (info) {
+    case "1":
+      prompt(" Add Your Income");
+      
+      break;
+    case "2":
+      prompt("Add Your Expenses");
+      
+      break;
+    case "3":
+        prompt("What was your expense? (rent/gas/food etc)");
+      
+      break;
+    case "4":
+        prompt("Get Summary");
+      // Don't know how to store the answers!!!
+      break;
+    default:
+        prompt("Invalid selection");
+      break;
+  }
+
+
+//this was working but then I changed it to switch:
+// if(choice = 1){
+//        const choice = prompt(
+//         "Add your income"
+//        ) 
+//     }; 
+
+
+//3. WE NEED A FUNCTION FOR THE EXPENSES:
+//DON'T KNOW HOW TO WRITE IT:
+
+// function menu() {
+//     const choice = parseFloat(
+//         //NOTE TO MYSELF: parseFloat() The parseFloat() 
+//         //function parses a string argument and returns a floating point number.
+//         prompt(
+           
+//         )
+//     );
+//     console.log("choice: " + choice);
+// }
+
+// //we shopuld call it out: 
+// menu()
