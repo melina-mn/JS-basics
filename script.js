aa94d9d43a550cb12ac09182c84df679965d549f
Melina-mn <142990929+Melina-mn@users.noreply.github.com>
2023-11-22 18:47:33 +0100

    prompts addded

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+
+
+
+
+
+
+
 // THE ACCOUNT OBJECT SHOULD HAVE THE FOLLWOING PROPERTIES:
 // name: the account holders name, should be a string
 // expenses: an array that should hold all of your expenses, can be empty from start
 // income: an array that should hold all of your income, can be empty from start
 // addExpenses: a function that vill add an expense to the expenses array
-// addIncome: a function that vill add an income to the income array
+// addIncome: a function that will add an income to the income array
 // listAllExpenses: a function that should list all the expenses in the expenses array
 // getSummary: a function that should summarize your total balances. It should show your total
 // income, your total expenses and also calculate your current balances (income - expenses).
@@ -20,66 +27,39 @@
 
 
 // TO DO LIST:
-// 1. CREATE AN OBJECT FOR YOUR WEBSITE THAT HOLDS (NAME, EXPENSES, INCOME, ADDEXPENSES,ADDINCOME, LISTALLEXPENSES, AND A SUMMERY)
-const melinaObject = {
-    name: "Melina",
-    expenses: [],
-    income: [],
-    addExpenses : function(){
-        prompt(
-                    'What was your expense? (rent/gas/food etc)'
-                );
-        }
-}
 
-const expensesSave = prompt(
-    "What was your expense? (rent/gas/food/clothes etc)"
-)
-
-//2. WE NEED A FUNCTION MENU:
+//1. WE NEED A FUNCTION MENU:
 
 function menu() {
-    const questions = parseFloat(
-        prompt(
-          "Please select a choice :\n 1. Add income\n 2. Add expense\n 3. List all expenses\n 4. Get summary"
-        )
-      );
+  const questions = parseFloat(
+      prompt(
+        "Please select a choice :\n 1. Add income\n 2. Add expense\n 3. List all expenses\n 4. Get summary"
+      )
+    );
 }
+
 
 //we should call it out: 
 menu()
 
+// 2. CREATE AN OBJECT FOR YOUR WEBSITE THAT HOLDS (NAME, EXPENSES, INCOME, ADDEXPENSES,ADDINCOME, LISTALLEXPENSES, AND A SUMMERY)
+const melinaObject = {
+  name: "Melina",
+  expenses:["gas", "food", "rent", "clothes"],
+  income: [],
+}
 
-// 3. ADD SWITCH:
-// switch (info) {
-//     case "1":
-//       prompt(" Add Your Income");
-      
-//       break;
-//     case "2":
-//       prompt("Add Your Expenses");
-      
-//       break;
-//     case "3":
-//         prompt("What was your expense? (rent/gas/food etc)");
-      
-//       break;
-//     case "4":
-//         prompt("Get Summary");
-//       // Don't know how to store the answers!!!
-//       break;
-//     default:
-//         prompt("Invalid selection");
-//       break;
-//   }
+const income = parseFloat(prompt(
+  "How much is your income?"
+  ));
 
+const expenses = parseFloat(prompt(
+  "Add your expenses"
+));
 
-//this was working but then I changed it to switch:
-// if(choice = 1){
-//        const choice = prompt(
-//         "Add your income"
-//        ) 
-//     }; 
-
+let expensesSave = parseFloat(prompt(
+  "What was your expense? (\nrent\ngas\nfood\nclothes)"
+));
+alert(summery) 
 
 
