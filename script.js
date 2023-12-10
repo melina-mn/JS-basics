5db835371accb38b7014e4bf73a2b34501fd8573
Melina-mn <142990929+Melina-mn@users.noreply.github.com>
2023-12-10 13:21:18 +0100

    summary function fixed

--- script.js
+++ script.js
@@ -34,40 +34,81 @@ const account = {
     const category = prompt(
       "What was your expense? (\nrent\ngas\nfood\nclothes)"
     );
+
     const amount = parseFloat(prompt("How much is your income?"));
+
     this.expenses.push({ expensesCategory: category, expenseAmount: amount });
+
     // const expensesAmount = parseFloat(prompt("How much is your expenses"));
     // console.log(this.expenses);
     menu();
   },
   addIncome: function () {
     const amount = parseFloat(prompt("How much is your income?"));
+
     this.incomes.push({
-      income: amount,
+      incomesAmount: amount,
     });
     menu();
   },
   listAllExpenses: function () {
-    for(let i = 0; i < this.expenses.length; i++){
-    let value = expenses[i].category +
-    incomes[i].amount;
-        console.log(value);
-    };
-     
+    // 1. we want to list all expenses we added in our array for the user
+    // 2. in order to list all elements in a array we need a loop (foreach)
+    // 3. each expense should be shown on one row with the category and amount
+    // 4. we want the list shows in an alert
+
+    let message = "";
+    // we need to declare message here so we can pass that to the alert
+
+    this.expenses.forEach(function (expense) {
+      // expense inside () is going to represent ONE expense in our exoenses array
+      // so javascript that expense inside () is actually an object
+
+      // we are going to build a custom message just a normal string with our properties
+
+      message += 
+        `Expense category: ${expense.expensesCategory}
+Expense amount: ${expense.expenseAmount}\n`;
+
+      //alert("Expense category " + expense.expensesCategory);
+      // this show ONE alert for EACH category which is not what we want
+    
+    });
+    alert(message);
+
     menu();
   },
 
-
-
-
-  // when we have answered all the questions we store it by using the code alert and the ${} template string:
-  /*
+  
   summary: function () {
-    alert(`Total balances: ${account.}
-    Total Incomes: ${account.}
-    Total expenses: ${account.}`)
-  },
-  */
+    // 1. calculate total incomes - total expenses and show the result
+
+    // 2. first we need to calculate the total incomes and save it in a variable, hint: you need to use a loop  
+    
+    // 4. with this said we need two variables, one for total incomes and one for total expenses
+    let totalIncomes = 0;
+    let totalExpenses = 0; 
+ 
+    this.incomes.forEach(function (income) {
+      totalIncomes += income.incomesAmount
+    });
+    // 3. we need to do the same for expenses, keep in mind that you only are interested in expense.expenseAmount not the category
+    this.expenses.forEach(function (expense){
+    totalExpenses += expense.expenseAmount
+  }); 
+
+    // 5. then we need to create a new variable to store the result of totalIncomes - totalExpenses
+    let result = totalIncomes - totalExpenses;
+    alert(`Your Total Incomes: ${totalIncomes}
+Your Total Expenses: ${totalExpenses}
+Result: ${result}`);
+
+    
+  
+   
+    // 6. once that is done you can show the result to the user for example in an alert
+    
+}
 };
 
 //2. WE NEED A FUNCTION MENU:
@@ -77,8 +118,6 @@ function menu() {
       "Please select a choice :\n1.Add income\n2.Add expense\n3.List all expenses\n4. Get summary"
     )
   );
-    
-
 
   // if/else STATEMENT:
   if (choice === 1) {
@@ -89,63 +128,12 @@ function menu() {
     account.listAllExpenses();
   } else if (choice === 4) {
     account.summary();
-  } else if (choice < 0 && choice > 4 && choice != Number){
-    alert('Unvalid answer, choose a number between 1 to 4 please!');
+  } else if (choice < 0 && choice > 4 && choice != Number) {
+    alert("Unvalid answer, choose a number between 1 to 4 please!");
     menu();
   }
 }
 
 //we should call it out:
-    menu();
+menu();
 
-// function getSummary() {
-//     alert(`your income is ${melinaObject.income}your total expenses ${melinaObject.expenses}`);
-//     };
-
-// let answer = '';
-// if (answer === '1') {
-//   const income = parseFloat(prompt(
-//     'How much is your income?'
-//     ));
-// } else if (answer === '2'){
-//   const expenses = parseFloat(prompt(
-//     'Add your expenses'
-//   ));
-// } else if (answer === '3'){
-//   let expensesSave = parseFloat(prompt(
-//     'What was your expense? (\nrent\ngas\nfood\nclothes)'
-//   ));
-// }
-
-/* function incomeChoice() {
-  const income = parseFloat(prompt("How much is your income?"));
-  return menu();
-}
-incomeChoice();
-function expensesChoice() {
-  const expenses = parseFloat(prompt("Add your expenses"));
-  return menu();
-}
-expensesChoice();
-function expensesSaveChoice() {
-  let expensesSave = parseFloat(
-    prompt("What was your expense? (\nrent\ngas\nfood\nclothes)")
-  );
-  return menu();
-}
-expensesSaveChoice(); */
-// getSummary()
-
-
-/*
-
-  for (let i = amount; i < account.incomes; i++ ) {
-  
-    const incomes = account.addIncome[i];
-  }
-  account.incomes.push({
-    incomeCategory: category,
-    incomeAmount: amount
-    })
-  console.log(account.incomes);
-*/
